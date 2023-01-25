f11657935165ad1e762ca988501f846ae4997070
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2023-01-25 15:29:16 +0200

    Added logout button, navbar fixes

--- chatroomsite_frontend/src/app/app.component.ts
+++ chatroomsite_frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelperFunctionsService } from "./services/helper-functions.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatroomsite_frontend';
+
+  //For navbar
+  getCookie = HelperFunctionsService.getCookie;
+  deleteCookie = HelperFunctionsService.deleteCookie;
 }
